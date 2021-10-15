44c875c3b25dca688cdf4274e84be3454b75eca0
Camron Gonzalez <camrong520@gmail.com>
2021-10-15 13:36:40 -0400

    Modeled two scenes (not Complete)/Teleport Trigger
    Finished modeling the city started the univeristy interior and made a teleport trigger to move the player to different places.

---
+++ Assets/trigger_this.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trigger_this : MonoBehaviour
+{
+    public AudioSource mySoundSource;
+    public float audioClipTime;
+
+    public int timesEntered;
+
+    public Transform warpPoint;
+    public GameObject firstPerson;
+    
+    void OnTriggerEnter()
+    {
+        //print("whats up dawg?");
+        //firstPerson.transform.position = warpPoint.transform.position;
+        mySoundSource.Play();
+        firstPerson.transform.position = warpPoint.transform.position;
+    }
+
+    void OnTriggerExit()
+    {
+        mySoundSource.Pause();
+
+        print("I wish for death");
+    }
+
+    void update()
+    {
+        audioClipTime = mySoundSource.time;
+    }
+}
